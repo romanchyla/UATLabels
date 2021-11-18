219ff14634e67bcc0d0c0c0dd26ba51d7fd19b75
Roman Chyla <roman.chyla@gmail.com>
2021-11-17 22:11:11 -0500

    Not quite, but close - can get weight which seems interesting; but problem in UnionFind

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
+import heapq
 from rprojc import StandardProject
-from cspatterns.datastructures import graphs
+from cspatterns.datastructures import graphs, unionfind
+from collections import defaultdict
 
 class Application(StandardProject):
     def __init__(self):
@@ -9,7 +11,7 @@ class Application(StandardProject):
         self._str2id = {}
 
     def id2label(self, id:int) -> str:
-        return self._id2str.get(id, None)
+        return self._id2str[id]
 
     def label2id(self, label:str) -> int:
         if label not in self._str2id:
@@ -29,7 +31,7 @@ class Application(StandardProject):
 
         graph = graphs.WeightedUndirectedGraph()
         for line in open(datafile, 'r'):
-            parts = line.split('\t')
+            parts = list(filter(lambda x: x.strip(), line.strip().split('\t')))
             bibcode, labels = parts[0], parts[1:]
 
             for i in range(len(labels)+1):
@@ -47,15 +49,17 @@ class Application(StandardProject):
         cc = 0
         visited = {}
 
-        def dfs(v, seen, cc):
-            seen[v] = cc
-            for w, _ in graph.adj(v):
-                if w not in seen:
-                    dfs(w, seen, cc)
-
         for v in graph.vertices():
             if v not in visited:
-                dfs(v, visited, cc)
+                stack = [v]
+                while stack:
+                    v = stack.pop()
+                    if v in visited:
+                        continue
+                    visited[v] = cc
+                    for w, _ in graph.adj(v):
+                        if w not in visited:
+                            stack.append(w)
                 cc += 1
 
         if cc == 1:
@@ -74,7 +78,57 @@ class Application(StandardProject):
                     grs[ic].add(v, w, weight)
         
         return grs
+
+    def dump_graph(self, graph, location):
+        with open(location, 'w') as fo:
+            for v,w,weight in graph.edges():
+                fo.write('{}\t{}\t{}\n'.format(self.id2label(v), self.id2label(w), weight))
         
+
+    def split_graph(self, graph, stop_at=2):
+        """
+        Basically, we are going to use Kruskal alg to identify 
+        minimum spanning trees (the most frequent edges have the
+        smallest weights). We'll stop once we have reached 'stop_at'
+        number of components"""
+
+        union = unionfind.UnionFind()
+        pq = []
+        for v,w,weight in graph.edges():
+            pq.append((weight, v, w))
+            union.get_key(v)
+            union.get_key(w)
+        heapq.heapify(pq)
+
+        
+        
+        while pq:
+            weight, v, w = heapq.heappop(pq)
+            if not union.is_connected(v, w):
+                union.join(v, w)
+                #print('union_size={}'.format(union.num_components()))
+                #if union.num_components() <= stop_at:
+                stop_at -= 1
+                if not stop_at:
+                    print('Stopping while len(pq)={}, union_size={}'.format(len(pq), union.num_components()))
+                    break
+
+        # now what remains is to read the union and 
+        # reconstruct the spanning tree
+        data = union.compress()
+        mst = defaultdict(graphs.WeightedUndirectedGraph)
+        seen = {}
+        print(set(data), len(data))
+        for i, parent in enumerate(data):
+            for w, weight in graph.adj(i):
+                if data[w] != parent:
+                    continue  # this edge is not part of the same MST
+                mst[parent].add(i, w, weight)
+
+        print(mst)
+        return mst.values()
+
+
 
 
 def test():
@@ -83,12 +137,22 @@ def test():
     g = a.load_labels(datafile = a.config.get('UAT_DATA', 'workdir/uat.test'))
     print('Loaded UAT graph, vertices: {}, edges: {}'.format(g.num_vertices(), g.num_edges()))
 
-    print('Going to identify separate clusters (if any)')
+    print('Going to identify separate graphs (if any)')
 
     ccs = a.find_connected_components(g)
     print('Found {} connected components'.format(len(ccs)))
 
+    workdir = a.config['WORKDIR']
+    split = a.config.get('NUM_SUBGRAPHS')
     for ic, cc in enumerate(ccs):
         print('{} vertices: {}, edges: {}'.format(ic, cc.num_vertices(), cc.num_edges()))
+        loc = workdir + '/graph.{}'.format(ic)
+        a.dump_graph(cc, loc)
+        print('-- written into: {}'.format(loc))
 
+        print('Going to split the graph into closest {} subgraphs'.format(split))
+        for iic, subgraph in enumerate(a.split_graph(cc, split)):
+            loc = workdir + '/graph.{}.{}'.format(ic, iic)
+            a.dump_graph(subgraph, loc)
+            print('-- written: {}'.format(loc))
     
